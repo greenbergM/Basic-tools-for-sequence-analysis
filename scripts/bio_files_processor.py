c6a710fc8c1bb6fc830dba0c553798d8d0ca3948
Greenberg Michael <greenerkk@gmail.com>
2023-10-13 18:30:33 +0300

    Update convert_multiline_fasta

--- scripts/bio_files_processor.py
+++ scripts/bio_files_processor.py
@@ -1,4 +1,7 @@
-def convert_multiline_fasta_to_oneline(input_fasta, output_fasta):
+import os
+
+
+def convert_multiline_fasta_to_oneline(input_fasta, output_fasta=None):
     oneline_fasta_dict = {}
     seq = ''
     with open(input_fasta, mode='r') as multiline_fasta:
@@ -10,11 +13,12 @@ def convert_multiline_fasta_to_oneline(input_fasta, output_fasta):
                     seq += (line.strip('\n'))
                 oneline_fasta_dict[name] = seq
                 seq = ''
-
-    with open(output_fasta, mode='w') as oneline_fasta:
+    if output_fasta is None:
+        output_fasta = 'oneline_' + os.path.basename(os.path.realpath(input_fasta))
+    else:
+        output_fasta += '.fasta'
+    location = os.path.dirname(os.path.realpath(input_fasta))
+    with open(os.path.join(location, output_fasta), mode='w') as oneline_fasta:
         for name, seq in oneline_fasta_dict.items():
             oneline_fasta.write(name)
             oneline_fasta.write(seq + '\n')
-
-
-
