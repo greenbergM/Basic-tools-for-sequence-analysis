831a4e3a476867114fd7b4f57fd60d1b8172b368
Greenberg Michael <greenerkk@gmail.com>
2023-10-13 20:56:59 +0300

    Enhance convert_multiline func in bio_files_processor.py

--- scripts/bio_files_processor.py
+++ scripts/bio_files_processor.py
@@ -1,24 +1,26 @@
 import os
 
 
-def convert_multiline_fasta_to_oneline(input_fasta, output_fasta=None):
-    oneline_fasta_dict = {}
-    seq = ''
-    with open(input_fasta, mode='r') as multiline_fasta:
-        for line in multiline_fasta:
-            if line.startswith('>'):
-                name = line
-            else:
-                while not line.startswith('>'):
-                    seq += (line.strip('\n'))
-                oneline_fasta_dict[name] = seq
-                seq = ''
+def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta=None):
+    """
+    Creates fasta file with oneline sequences based on given fasta file with multiline sequences in the same directory.
+    :param input_fasta: path to the multiline fasta file (str)
+    :param output_fasta: name of oneline fasta file (str)
+    """
     if output_fasta is None:
-        output_fasta = 'oneline_' + os.path.basename(os.path.realpath(input_fasta))
-    else:
+        output_fasta = f'oneline_{os.path.basename(os.path.realpath(input_fasta))}'
+    if not output_fasta.endswith('.fasta'):
         output_fasta += '.fasta'
+
     location = os.path.dirname(os.path.realpath(input_fasta))
-    with open(os.path.join(location, output_fasta), mode='w') as oneline_fasta:
-        for name, seq in oneline_fasta_dict.items():
-            oneline_fasta.write(name)
-            oneline_fasta.write(seq + '\n')
+    seq = ''
+    with open(input_fasta, mode='r') as fr, open(os.path.join(location, output_fasta), mode='w') as fw:
+        for line in fr:
+            if line.startswith('>'):
+                if seq:
+                    fw.write(seq + '\n')
+                    seq = ''
+                fw.write(line)
+            else:
+                seq += line.strip('\n')
+        fw.write(seq)
