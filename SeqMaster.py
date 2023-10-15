cb6b24aa9d14452f5e25ef2413cb9f8ef529fa99
Greenberg Michael <greenerkk@gmail.com>
2023-10-15 13:52:26 +0300

    Replace convert_multiline_fasta func to SeqMaster.py from bio_files_processor.py

--- SeqMaster.py
+++ SeqMaster.py
@@ -109,6 +109,31 @@ def run_filter_fastaq(input_path: str, output_filename=None, gc_bounds=(0, 100),
     print(f'Filtering completed; {len(filtered_seqs.keys())} sequences were selected from {len(seqs.keys())} given.')
 
 
+def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta=None):
+    """
+    Creates fasta file with oneline sequences based on given fasta file with multiline sequences in the same directory.
+    :param input_fasta: path to the multiline fasta file (str)
+    :param output_fasta: name of oneline fasta file (str)
+    """
+    if output_fasta is None:
+        output_fasta = f'oneline_{os.path.basename(os.path.realpath(input_fasta))}'
+    if not output_fasta.endswith('.fasta'):
+        output_fasta += '.fasta'
+
+    location = os.path.dirname(os.path.realpath(input_fasta))
+    seq = ''
+    with open(input_fasta, mode='r') as fr, open(os.path.join(location, output_fasta), mode='w') as fw:
+        for line in fr:
+            if line.startswith('>'):
+                if seq:
+                    fw.write(seq + '\n')
+                    seq = ''
+                fw.write(line)
+            else:
+                seq += line.strip('\n')
+        fw.write(seq)
+
+
 def select_genes_from_gbk_to_fasta(*genes: str, input_gbk: str, n_before: int, n_after: int, output_fasta=None):
     """
     Creates fasta file with neighbor CDSs to given genes from GBK file and stores it
--- scripts/bio_files_processor.py
+++ scripts/bio_files_processor.py
@@ -1,31 +1,3 @@
-import os
-
-
-def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta=None):
-    """
-    Creates fasta file with oneline sequences based on given fasta file with multiline sequences in the same directory.
-    :param input_fasta: path to the multiline fasta file (str)
-    :param output_fasta: name of oneline fasta file (str)
-    """
-    if output_fasta is None:
-        output_fasta = f'oneline_{os.path.basename(os.path.realpath(input_fasta))}'
-    if not output_fasta.endswith('.fasta'):
-        output_fasta += '.fasta'
-
-    location = os.path.dirname(os.path.realpath(input_fasta))
-    seq = ''
-    with open(input_fasta, mode='r') as fr, open(os.path.join(location, output_fasta), mode='w') as fw:
-        for line in fr:
-            if line.startswith('>'):
-                if seq:
-                    fw.write(seq + '\n')
-                    seq = ''
-                fw.write(line)
-            else:
-                seq += line.strip('\n')
-        fw.write(seq)
-
-
 def get_cds_list(input_gbk: str) -> list:
     """
     Creates list of all CDS from GBK file.
