2641257dee40ca85fef163192aeca467d5d3be1b
Greenberg Michael <greenerkk@gmail.com>
2023-10-17 19:58:19 +0300

    Add check for negative n_before values in select_genes_from_gbk func in bio_files_processor.py

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -35,7 +35,7 @@ def select_genes_from_gbk_to_fasta(*genes: str, input_gbk: str, n_before: int, n
     :param input_gbk: path to GBK file (str)
     :param n_before: number of neighbor CDSs before gene of interest (int)
     :param n_after: number of neighbor CDSs after gene of interest (int)
-    :param output_fasta: name of the output fasta file
+    :param output_fasta: name of the output fasta file (str)
     """
     cds_list = bfp.get_cds_list(input_gbk)
     gene_cds_dict = bfp.get_gene_to_cds_dict(input_gbk)
@@ -45,6 +45,8 @@ def select_genes_from_gbk_to_fasta(*genes: str, input_gbk: str, n_before: int, n
     for gene in genes:
         gene_cds = gene_cds_dict[gene]
         gene_position = cds_list.index(gene_cds)
+        if gene_position - n_before < 0:
+            raise ValueError(f'CDCs before {gene} are out of bounds! Use other value for n_before.')
         cds_before = cds_list[gene_position - n_before:gene_position]
         cds_after = cds_list[gene_position + 1:gene_position + n_after + 1]
         cds_of_interest = cds_of_interest + cds_before + cds_after
