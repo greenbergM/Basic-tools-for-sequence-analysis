5b13bda5c74936d52a541e24c031c074cfeac5bb
Greenberg Michael <greenerkk@gmail.com>
2024-04-30 23:47:19 +0300

    Add custom_random_forest.py

---
+++ custom_random_forest.py
@@ -0,0 +1,69 @@
+from multiprocessing import Pool
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.tree import DecisionTreeClassifier
+
+
+class RandomForestClassifierCustom(BaseEstimator):
+    def __init__(
+        self, n_estimators=10, max_depth=None, max_features=None, random_state=None
+    ):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.max_features = max_features
+        self.random_state = random_state
+
+        self.trees = []
+        self.feat_ids_by_tree = []
+
+    def _fit_tree(self, i, random_state, max_features, max_depth, X, y):
+        np.random.seed(random_state + i)
+        feat_idx = np.random.choice(X.shape[1], max_features, replace=False)
+
+        bootstrap_idx = np.random.choice(X.shape[0], X.shape[0], replace=True)
+        bootstrap_X = X[bootstrap_idx]
+        bootstrap_y = y[bootstrap_idx]
+
+        tree = DecisionTreeClassifier(
+            max_depth=max_depth, random_state=(random_state + i)
+        )
+        tree.fit(bootstrap_X[:, feat_idx], bootstrap_y)
+
+        return tree, feat_idx
+
+    def fit(self, X, y, n_jobs=1):
+        self.classes_ = sorted(np.unique(y))
+
+        args = [
+            (i, self.random_state, self.max_features, self.max_depth, X, y)
+            for i in range(self.n_estimators)
+        ]
+
+        with Pool(n_jobs) as p:
+            results = p.starmap(self._fit_tree, args)
+
+        self.trees, self.feat_ids_by_tree = zip(*results)
+
+        return self
+
+    def _predict_proba_tree(self, tree_feat, X):
+        tree, feat_idx = tree_feat
+        return tree.predict_proba(X[:, feat_idx])
+
+    def predict_proba(self, X, n_jobs=1):
+        with Pool(n_jobs) as p:
+            probas_per_tree = p.starmap(
+                self._predict_proba_tree,
+                [
+                    (tree_feat, X)
+                    for tree_feat in zip(self.trees, self.feat_ids_by_tree)
+                ],
+            )
+
+        return np.mean(probas_per_tree, axis=0)
+
+    def predict(self, X, n_jobs=1):
+        probas = self.predict_proba(X, n_jobs)
+        predictions = np.argmax(probas, axis=1)
+
+        return predictions
